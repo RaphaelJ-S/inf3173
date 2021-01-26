891d63593c1e9157d565157c758711103725338b
RaphaelJ-S <dk391069@ens.uqam.ca>
2021-01-26 11:07:55 -0500

    lab02 finit

--- lab02/prime.c
+++ lab02/prime.c
@@ -1,7 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 
 /*
 ldd prime : pour trouver toutes les lib dynamiquement liées lors de la compilation de prime
@@ -19,7 +19,7 @@ int main(int _argc, char** _argv) {
 		return -1;	
 	}
 	
-	cible = strtol(_argv[1], NULL, sizeof(long int));	
+	cible = strtol(_argv[1], NULL,10); 
 	if(cible == 0) {
 		puts("\nArgument invalide\n");
 		return -1;
@@ -33,10 +33,39 @@ int main(int _argc, char** _argv) {
 
 size_t prime(const long int _cible){
 	size_t cap = 10;
-	long int *tabPrime = malloc(sizeof(long int) * cap);
+	long int *tabPrime = (long int*)malloc(sizeof(long int) * cap);
+	bool prime;
+
+	if(tabPrime == NULL) { 
+		perror("allocation");
+		return -1;
+	}
 	size_t nbrPrime = 0;
 
+	for (long int i = 2 ; i <= _cible ; ++i) {
+		prime = true;
+		for( size_t k = 0 ; k < nbrPrime ; ++k ) {
+			long int j = tabPrime[k];
+			if (j > sqrt(i)) break;
 
+			if (i%j == 0) {
+				prime = false;
+				break;
+			}	
+		}
+		if( prime ) {
+			tabPrime[nbrPrime] = i;
+			nbrPrime++;
+			if ( nbrPrime == cap ) {
+				cap *= 2;
+				tabPrime = (long int*)realloc(tabPrime, cap * sizeof(long int));
+				if ( tabPrime == NULL ) { 
+					perror("Allocation");
+					return -1;
+				}
+			}
+		}	
+	}
 	free(tabPrime);
 	return nbrPrime;	
 }
